ba380d8f20cbe19c36adc6445c9993606205f980
Viktors Jenovs <jviktors@gmail.com>
2018-08-16 19:54:26 +0300

    Add error handling to scraper

--- server/server.js
+++ server/server.js
@@ -15,28 +15,42 @@ app.use((req, res, next) => {
 app.get('/nl/:term', (req, res) => {
   fetch(
     `https://www.vandale.nl/gratis-woordenboek/nederlands/betekenis/${
-    req.params.term
+      req.params.term
     }`
   )
     .then(res => res.text())
     .then(text => {
-      const $ = cheerio.load(text);
-      res.send($('#content-area .snippets').html());
+      res.send(getContent(text, req.params.term));
+    })
+    .catch(err => {
+      res.send(`<div>Failed to fetch.<br>${err}.<br>Try again later</div>`);
     });
 });
 
 app.get('/en/:term', (req, res) => {
   fetch(
     `https://www.vandale.nl/gratis-woordenboek/nederlands-engels/vertaling/${
-    req.params.term
+      req.params.term
     }`
   )
     .then(res => res.text())
     .then(text => {
-      const $ = cheerio.load(text);
-      res.send($('#content-area .snippets').html());
+      res.send(getContent(text, req.params.term));
+    })
+    .catch(err => {
+      res.send(`<div>Failed to fetch.<br>${err}.<br>Try again later</div>`);
     });
 });
+
+function getContent(text, term) {
+  const $ = cheerio.load(text);
+
+  const content = $('#content-area .snippets').html();
+
+  return !content.length
+    ? `<div>Search term not found: "${term}"</div>`
+    : content;
+}
 
 app.listen(3001, () => {
   console.log('Server listening on port 3001');
